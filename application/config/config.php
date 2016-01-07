a2a70e15c0f672af3f4475ab5d4d39d055acfe3f
bjmayor <415074476@qq.com>
2016-01-08 00:08:44 +0800

    add adminlte

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://www.xiaojiaoluo.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -370,7 +370,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = "Users/shunpingliu/workstation/www/testYou/application/session/";
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
--- application/controllers/admin/_Original.php
+++ application/controllers/admin/_Original.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Original extends Public_Controller {
+class Original extends Admin_Controller {
 
     public function __construct()
     {
