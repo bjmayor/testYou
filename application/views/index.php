329c4ac2b15020889cec9dda566f8ae4c1f17923
bjmayor <415074476@qq.com>
2016-01-14 16:19:11 +0800

    增加连接

--- application/views/index.php
+++ application/views/index.php
@@ -11,15 +11,15 @@
 <body>
 <section class="main">
     <nav class="navmeun">
-    	<div class="navevery"><i class="icon navicon chara"></i>性格</div>
-        <div class="navevery"><i class="icon navicon lover"></i>爱情</div>
-        <div class="navevery"><i class="icon navicon social"></i>社交</div>
-        <div class="navevery"><i class="icon navicon wealth"></i>财富</div>
-        <div class="navevery"><i class="icon navicon job"></i>职场</div>
-        <div class="navevery"><i class="icon navicon interest"></i>趣味</div>
-        <div class="navevery"><i class="icon navicon ability"></i>能力</div>
-        <div class="navevery"><i class="icon navicon iq"></i>智商</div>
-        <div class="navevery"><i class="icon navicon colligate"></i>综合</div>
+    <div class="navevery"><i class="icon navicon chara"></i><a href="<?php echo site_url("home/top/1");?>">性格</a></div>
+    <div class="navevery"><i class="icon navicon lover"></i><a href="<?php echo site_url("home/top/2");?>">爱情</a></div>
+        <div class="navevery"><i class="icon navicon social"></i><a href="<?php echo site_url("home/top/3");?>">社交</a></div>
+        <div class="navevery"><i class="icon navicon wealth"></i><a href="<?php echo site_url("home/top/4");?>">财富</a></div>
+        <div class="navevery"><i class="icon navicon job"></i><a href="<?php echo site_url("home/top/5");?>">职场</a></div>
+        <div class="navevery"><i class="icon navicon interest"></i><a href="<?php echo site_url("home/top/6");?>">趣味</a></div>
+        <div class="navevery"><i class="icon navicon ability"></i><a href="<?php echo site_url("home/top/7");?>">能力</a></div>
+        <div class="navevery"><i class="icon navicon iq"></i><a href="<?php echo site_url("home/top/8");?>">智商</a></div>
+        <div class="navevery"><i class="icon navicon colligate"></i><a href="<?php echo site_url("home/top/9");?>">综合</a></div>
     </nav>
     <ul class="screen">
             <li class="current-l">最新<i class="icon icon-b"></i></li>
@@ -31,7 +31,7 @@
     <li><a href="#"><div class="pros"><div class="pic"><img src="<?php echo base_url($front_dir);?>/images/pro.jpg"></div><div class="b_cont"><div class="texts">你的心灵到底什么颜色？</div><div class="star-popu"><span class="star"><i class="icon star-s"></i><i class="icon star-s"></i><i class="icon star-s"></i><i class="icon star-s"></i><i class="icon star-s"></i></span><span class="popu">人气：17400</span><span class="getinto"><i class="icon into"></i></span></div></div></div></a></li>
     <li><a href="#"><div class="pros"><div class="pic"><img src="<?php echo base_url($front_dir);?>/images/pro.jpg"></div><div class="b_cont"><div class="texts">你的心灵到底什么颜色？</div><div class="star-popu"><span class="star"><i class="icon star-s"></i><i class="icon star-s"></i><i class="icon star-s"></i><i class="icon star-s"></i><i class="icon star-s"></i></span><span class="popu">人气：17400</span><span class="getinto"><i class="icon into"></i></span></div></div></div></a></li>
     </ul>
-    <div class="pages"><a class="buts" href="#">下一页</a></div>	
+    <div class="pages"><a class="buts" href="#">下一页</a></div>    
     <div class="pages"><a class="buts-last" href="#">上一页</a><a class="buts-next" href="#">下一页</a></div>
 </section>
 </body>
